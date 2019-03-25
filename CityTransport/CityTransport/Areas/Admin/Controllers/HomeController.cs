aecc9dcb6418244749c5b8759407b6865d5e3af3
mboncheva <m_boncheva@abv.bg>
2019-03-25 20:13:10 +0200

    Added Bootstrap and AdminLayout

---
+++ CityTransport/CityTransport/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿namespace CityTransport.Web.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : AdminController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
